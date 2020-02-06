8b909df65c73accb1323da40e21c540bac69edc7
Guido Valenzuela <nucleico@gmail.com>
2020-02-06 18:04:54 -0300

    Updates

---
+++ precache-manifest.1ff53a3b3cd91d5638e85e9d318a7e3d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fe5fc393975193c477a0800027fb6a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1496dff22034c70b7a16",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "cee6146ccda4d00f7fed",
+    "url": "/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "1496dff22034c70b7a16",
+    "url": "/static/js/2.8ec5975b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.8ec5975b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cee6146ccda4d00f7fed",
+    "url": "/static/js/main.17369537.chunk.js"
+  },
+  {
+    "revision": "263f93dc1972f75175de",
+    "url": "/static/js/runtime-main.1986da33.js"
+  },
+  {
+    "revision": "f4a81a7875c4a4fbac95f0256c751956",
+    "url": "/static/media/am.f4a81a78.jpeg"
+  },
+  {
+    "revision": "df17bc388e7967a12178b70f1c38a66a",
+    "url": "/static/media/az.df17bc38.jpg"
+  },
+  {
+    "revision": "50f256ca8bc683b0de5ffefd25205531",
+    "url": "/static/media/dt.50f256ca.jpg"
+  },
+  {
+    "revision": "7c53ccdd9a2c6cad142a0101112540c3",
+    "url": "/static/media/gm.7c53ccdd.jpeg"
+  },
+  {
+    "revision": "fd6d614fe0cee1db7b53b2a9e0559ce6",
+    "url": "/static/media/jd.fd6d614f.jpeg"
+  },
+  {
+    "revision": "f373c5090bb15873bf5f1d8eefef70bd",
+    "url": "/static/media/nd.f373c509.jpeg"
+  },
+  {
+    "revision": "7ecd38116289c09c2bb39bed65e418d4",
+    "url": "/static/media/rf.7ecd3811.jpg"
+  },
+  {
+    "revision": "e47725409fb39e16b58d600dcfbbc0c7",
+    "url": "/static/media/rn.e4772540.jpeg"
+  },
+  {
+    "revision": "d1d202cfe58a405ccc15111bb1a57a37",
+    "url": "/static/media/st.d1d202cf.jpg"
+  }
+]);
