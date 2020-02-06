05173cb3367f9a63807cf5780aac56d070d4edb0
Guido Valenzuela <nucleico@gmail.com>
2020-02-06 01:57:35 -0300

    js files added

---
+++ scripts/portfolioScript.js
@@ -0,0 +1,42 @@
+const apps = [
+  {
+    name: 'TodoList',
+    description: 'Agregar actividades, hacerlas, seguir',
+    img: 'todo.png',
+    route: 'Todo%20List/todo.html',
+    class: 'todoApp'
+  },
+  {
+    name: 'TipApp',
+    description: 'Calculá tus propinas.',
+    img: 'tip.png',
+    route: 'tip/tip.html',
+    class: 'tipApp'
+  },
+  {
+    name: 'StarWars',
+    description: 'Enfrentá a tus personajes favoritos de Star Wars.',
+    img: 'starwars.png',
+    route: 'Star%20Wars/index.html',
+    class: 'starWarsApp'
+  },
+  {
+    name: 'Word',
+    description: 'Completá frases en el menor tiempo posible',
+    img: 'word.png',
+    route: 'Countdown/index.html',
+    class: 'wordApp'
+  }
+];
+
+const appsArr = apps
+  .map(app => {
+    return `<a class="linkApp" href="apps/${app.route}"> <div class="zone ${app.class}"><img id="appImg" src="./img/${app.img}" alt=""><span class="textApp">${app.description}</span> </div> </a>`;
+  })
+  .join('');
+
+const container = document.querySelector('.container');
+const appsDiv = document.createElement('div');
+appsDiv.innerHTML = appsArr;
+const appsArrF = container.appendChild(appsDiv);
+appsArrF.classList.add('gridRow');
---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+const navClassUp = document.querySelector('.navClassUp');
+const navDiv = document.createElement('div');
+navDiv.innerHTML = `<nav class="navClass">
+<ul>
+ <li><a class="link" href="index.html"><span class="op">Home</span></a></li>
+ <li><a class="link" href="about.html"><span class="op">About</span></a></li>
+ <li class="navHover link"><a class="link" href="portfolio.html"><span class="op">Portfolio</span></a>
+   <ul class="sub-menu">
+     <li><a class="link" href="apps/tip/tip.html">Tip Calculator</a></li>
+     <li> <a class="link" href="apps/Todo%20List/todo.html">Todo List</li>
+     <li><a class="link" href="apps/Star%20Wars/index.html">Star Wars App</li>
+       <li><a class="link" href="apps/Countdown/index.html">Word App</li>
+   </ul></li>
+ <li><a class="link" href="contact.html"><span class="op">Contacto</span></a></li>
+</ul>
+<hr id="hr"> 
+</nav>`;
+const navDivF = navClassUp.appendChild(navDiv);
+navDivF.classList.add('navClass');
+
+const lateralBar = document.querySelector('.lateralBar');
+const lateralBarDiv = document.createElement('div');
+lateralBarDiv.innerHTML = `<div class="cont">
+<a href="#" class="fa fa-facebook"></a>
+<a href="https://github.com/nucleico/Web-Personal" class="fa fa-github"></a>
+<a href="#" class="fa fa-google"></a>
+<a href="https://www.linkedin.com/in/guido-valenzuela-575658173" class="fa fa-linkedin"></a>
+<a href="#" class="fa fa-youtube"></a>
+</div> `;
+const lateralBarDivF = lateralBar.appendChild(lateralBarDiv);
+lateralBarDivF.classList.add('cont');
