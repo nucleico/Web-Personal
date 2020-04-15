c33dd7484b1f2b46305be72bcd0db97b80ff6256
Guido Valenzuela <nucleico@gmail.com>
2020-04-14 23:08:32 -0300

    portfolio updated

--- script/portfolio.js
+++ script/portfolio.js
@@ -1,69 +1,59 @@
 const apps = [
   {
     name: 'Marvel App',
-    description:
-      'Buscar personajes y comics de Marvel (desarrollada en React con Marvel API).',
+    description: 'Buscar personajes de Marvel (desarrollada en React).',
     img: 'marvellogo.png',
     route: 'https://guarded-spire-04377.herokuapp.com/',
-    class: 'marvelApp'
+    class: 'marvelApp',
   },
   {
     name: 'Ta Te Ti',
     description: 'Simple juego de Ta Te Ti en JS.',
     img: 'tateti.png',
     route: 'apps/tictac/index.html',
-    class: 'tatetiApp'
-  },
-  {
-    name: 'TodoList',
-    description: 'Agregar actividades, hacerlas, seguir.',
-    img: 'todo.png',
-    route: 'apps/Todo%20List/todo.html',
-    class: 'todoApp'
-  },
-  {
-    name: 'TipApp',
-    description: 'Calculá tus propinas.',
-    img: 'tip.png',
-    route: 'apps/tip/tip.html',
-    class: 'tipApp'
-  },
-  {
-    name: 'StarWars',
-    description: 'Enfrentá a tus personajes favoritos de Star Wars.',
-    img: 'starwars.png',
-    route: 'apps/Star%20Wars/index.html',
-    class: 'starWarsApp'
+    class: 'tatetiApp',
   },
   {
     name: 'Word',
     description: 'Completá frases en el menor tiempo posible.',
     img: 'word.png',
     route: 'apps/Countdown/index.html',
-    class: 'wordApp'
+    class: 'wordApp',
   },
   {
     name: 'TennisSearch',
     description: 'Buscá tus tenistas (desarrollada en React)',
     img: 'tennis.png',
     route: 'https://nucleico.github.io/tennisApp1/',
-    class: 'tennisApp'
+    class: 'tennisApp',
   },
   {
-    name: 'Progress Bar',
-    description: 'Barra de progreso con setInterval',
-    img: 'progressBar.png',
-    route: 'apps/progressBar/index.html',
-    class: 'progressBarApp'
-  }
+    name: 'StarWars',
+    description: 'Enfrentá a tus personajes favoritos de Star Wars.',
+    img: 'starwars.png',
+    route: 'apps/Star%20Wars/index.html',
+    class: 'starWarsApp',
+  },
 ];
 
 const appsArr = apps
-  .map(app => {
-    return `<a class="linkApp" href="${app.route}"> <div class="eachApp ${app.class}"><img id="appImg" src="./img/${app.img}" alt=""><span class="textApp">${app.description}</span> </div> </a>`;
+  .map((app) => {
+    return ` <div class="card" >
+    <div class="imgBx">
+    <a href="${app.route}">
+      <img
+        src= "../img/${app.img}"
+        alt="images">
+        
+        </a>
+    </div>
+    <div class="details">
+      <h2>${app.name}<br /><span>${app.description}</span></h2>
+    </div>
+  </div>`;
   })
   .join('');
 
-const portfolioContainer = document.querySelector('.portfolioContainer');
+const portfolioContainer = document.querySelector('.boxContainer');
 portfolioContainer.innerHTML = appsArr;
 portfolioContainer.classList.add('gridRow');
--- script/script.js
+++ script/script.js
@@ -5,7 +5,6 @@ navBarLeftSelector.innerHTML = `
 <ul class="navBarLeftUl">
 <i class="fas fa-battery-full"></i>
 <li><a href="index.html">Home</a></li>
- <li><a href="about.html">Sobre mí</a></li>
  <li><a href="portfolio.html">Portfolio</a>
    </li>
  <li><a href="contact.html">Contacto</a></li>
@@ -19,9 +18,7 @@ const lateralBarSelector = document.querySelector('.lateralBar');
 lateralBarSelector.innerHTML = `<div class="lateralBarContainer">
 <a href="#" class="fab fa-facebook"></a>
 <a href="https://github.com/nucleico/projects" class="fab fa-github"></a>
-<a href="#" class="fab fa-google"></a>
 <a href="https://www.linkedin.com/in/guidovalenzuela/" class="fab fa-linkedin"></a>
-<a href="#" class="fab fa-youtube"></a>
 </div> `;
 
 // Barra superior y Burger
@@ -31,8 +28,8 @@ const burgerDiv = document.querySelector('.burgerDiv');
 const burgerLines = document.querySelectorAll('.burgerLines');
 let barFolded = true;
 
-burgerDiv.addEventListener('click', function() {
-  burgerLines.forEach(function(item) {
+burgerDiv.addEventListener('click', function () {
+  burgerLines.forEach(function (item) {
     item.classList.toggle('cruz');
     if (barFolded) {
       navBarLeft.style.transform = 'translate(100%, 0)';
